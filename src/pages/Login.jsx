3c2f4cd80cfa9122b896062575dad19a87ef1857
madeline-lei <madeline.lei@gmail.com>
2024-10-14 12:06:09 -0400

    made file

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+export default function Login() {
+    return (
+        <>
+            <main className="flex flex-col items-center justify-center p-6 mx-auto">
+                <div className="bg-white">
+
+                </div>
+                
+            </main>
+        </>
+    )
+}
