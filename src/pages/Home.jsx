04b223ff507677bce095152817ed88ee23f8dc5e
jpuka01 <johnpuka03@gmail.com>
2024-10-15 16:10:09 -0400

    Completed Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,27 @@
 import React from 'react';
 import dummyBg from '../assets/dummy-bg.png';
 
+const sections = [
+    {
+        imgSrc: dummyBg,
+        imgAlt: "dummy-bg1",
+        title: "Choose your class level",
+        description: "Take a quick assessment to see what your English level is like!",
+    },
+    {
+        imgSrc: dummyBg,
+        imgAlt: "dummy-bg2",
+        title: "Find a time and instructor",
+        description: "Browse our list of classes to find one that works!",
+    },
+    {
+        imgSrc: dummyBg,
+        imgAlt: "dummy-bg3",
+        title: "Start Learning!",
+        description: "Go to class and start learning English. You'll be fluent in no time!",
+    },
+];
+
 const Home = () => {
     return (
         <div className="min-h-screen">
@@ -16,13 +37,15 @@ const Home = () => {
             </div>
             <div className="bg-white p-4 mt-4">
                 <h1 className="text-4xl font-bold mb-10 text-center">How It Works</h1>
-                <div className="flex flex-col md:flex-row items-center justify-center px-4">
-                    <img src={dummyBg} alt="blank-image" className="mb-4 md:mb-0 md:mr-20 rounded-lg shadow-md" />
-                    <div className="px-10 text-center md:text-left">
-                        <h2 className="text-2xl font-semibold mb-5">Choose your class level</h2>
-                        <p className="text-lg mb-4">Take a quick assessment to see what your English level is like</p>
+                {sections.map((section, index) => (
+                    <div key={index} className={`flex flex-col md:flex-row items-center justify-evenly px-4 md:px-2 mb-4 ${index % 2 === 1 ? 'md:flex-row-reverse' : ''}`}>
+                        <img src={section.imgSrc} alt={section.imgAlt} className="mb-4 md:mb-8 md:w-1/3 rounded-lg shadow-md" />
+                        <div className={`px-2 md:px-0 text-center md:text-left md:w-1/3`}>
+                            <h2 className="text-3xl font-semibold mb-5">{section.title}</h2>
+                            <p className="text-lg mb-4">{section.description}</p>
+                        </div>
                     </div>
-                </div>
+                ))}
             </div>
         </div>
     );
