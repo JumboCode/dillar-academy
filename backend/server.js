7b6bdb35826c2de13bb2f373f2ead46b1c59c81a
myix765 <myix765@gmail.com>
2024-10-19 12:01:13 -0400

    added require true to user schema

--- backend/server.js
+++ backend/server.js
@@ -30,12 +30,12 @@ const Schema = mongoose.Schema
 
 // User Schema
 const UserSchema = new Schema({
-    firstName: String,
-    lastName: String,
-    email: String,
-    password: String,
-    isAdmin: Boolean,
-    username: String,
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true},
+    password: { type: String, required: true},
+    isAdmin: { type: Boolean, required: true},
+    username: { type: String, required: true},
 })
 const User = mongoose.model("users", UserSchema)
 
