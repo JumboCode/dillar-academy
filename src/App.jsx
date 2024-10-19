63fb616fe538d36f6ce2f0b9e6fecc490f00512f
epicfahimxd <40809642+epicfahimxd@users.noreply.github.com>
2024-10-19 14:27:45 -0400

    layout of signup and login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
-import Home from './pages/Home'
+// import Home from './pages/Home'
+import Login from './pages/Login'
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <div>
+      {/* <Home /> */}
+      <Login/>
+    </div>
   )
 }
 
