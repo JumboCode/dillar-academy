d55b0d39712b5d1e80354a8e940387d1989d507f
Yi Gu <2962915100@qq.com.com>
2024-10-24 21:57:44 -0400

    drop-down-wrapper

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -10,11 +10,16 @@ const Dropdown = ({ label, children }) => {
       //use onMouseEnter and onMouseLeave to track the mouse
       //If the mouse is on the menu, turn state to true, otherwise false
       onMouseEnter={() => setIsOpen(true)}
-      onMouseLeave={() => setIsOpen(false)}
+      onMouseLeave={() => {
+        // Set a 1-second delay before closing the menu
+        timeoutId = setTimeout(() => {
+          setIsOpen(false);
+        }, 1000);
+      }}
     >
       <button class="dropdown-button">{label}</button>
       {isOpen && (
-        <div class="absolute">
+        <div class="absolute text-right">
           {children}
         </div>
       )}
