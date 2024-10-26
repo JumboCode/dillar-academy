73c7683244f2e1c257c7db0d37008ec319d0b660
tony1759 <107494566+tony1759@users.noreply.github.com>
2024-10-25 23:04:03 -0400

    cleanup

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ const Schema = mongoose.Schema
 // User Schema
 const UserSchema = new Schema({
     firstName: { type: String, required: true },
-    lastName: { type: String, required: true }, 
+    lastName: { type: String, required: true },
     email: { type: String, required: true},
     password: { type: String, required: true},
     isAdmin: { type: Boolean, required: true},
