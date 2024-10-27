93bf0c6fdadaffa74d0ce20ab22e40c859a13005
John Puka <johnpuka03@gmail.com>
2024-10-27 15:56:50 -0400

    stash

---
+++ src/pages/Login.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { Link } from "wouter";
+
+export default function Login() {
+    const [formData, setFormData] = useState({
+        username: '', 
+        password: '',
+      })
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const { username, password } = formData
+        alert(`Form submitted with\nusername: ${username}\nand password: ${password}`)
+    }
+    
+    return (
+        <>
+            <main className="bg-blue-200 h-screen flex justify-center items-center">
+                <div className="bg-white w-2/3 md:1/2 lg:w-2/5 py-16 px-5 sm:px-20 rounded-lg shadow-2xl min-h-min">
+                    <h1 className="text-2xl sm:text-3xl mb-3">Login</h1>
+                    <h3 className="text-sm sm:text-base text-slate-400 mb-6">Don't have an account?
+                        <Link href="/SignUp" className="text-blue-400"> Sign up</Link> 
+                    </h3>
+
+                    <form method="POST" onSubmit={handleSubmit}>
+                        <input type="text"
+                        id="Username" 
+                        name="username" 
+                        defaultValue={formData.username} 
+                        onChange={handleChange} 
+                        className="my-4 border border-gray-500 rounded-md block w-full p-2 sm:p-2.5 text-sm sm:text-base placeholder-gray-500" 
+                        placeholder="Username" required/>
+
+                        <input type="password" 
+                        id="Password" 
+                        name="password" 
+                        defaultValue={formData.password} 
+                        onChange={handleChange} 
+                        className="my-4 border border-gray-500 rounded-md block w-full p-2 sm:p-2.5 text-sm md:text-base placeholder-gray-500" 
+                        placeholder="Password" required/>
+                        
+                        <button className="bg-gray-200 py-2 px-4 rounded-md my-2 text-sm sm:text-base" type="submit">Login</button>
+                    </form>
+                </div>
+            </main>
+        </>
+    )
+}
