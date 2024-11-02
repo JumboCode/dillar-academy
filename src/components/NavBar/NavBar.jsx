8b818dea64aefb35626e39f0c6e190d621213beb
myix765 <myix765@gmail.com>
2024-11-02 17:59:35 -0400

    cleanup navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -21,7 +21,6 @@ const NavBar = () => {
             <NavLink href="/contact" isMobile={false}>Contact</NavLink>
             <NavLink href="/classes" isMobile={false}>Classes</NavLink>
             <NavLink href="/signup" isMobile={false}>Sign Up</NavLink>
-            {/* <LanguageSelector /> */}
           </div>
 
           {/* Mobile menu button */}
@@ -47,28 +46,10 @@ const NavBar = () => {
             <NavLink href="/signup" isMobile={true}>Sign Up</NavLink>
         </div>
         <div className="pt-4 pb-3 border-t border-gray-200">
-          {/* <LanguageSelector mobile /> */}
         </div>
       </div>
     </nav>
   );
 };
-
-// const LanguageSelector = ({ mobile }) => (
-//   <div className={`relative ${mobile ? '' : ''}`}>
-//     <button
-//       type="button"
-//       className="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 focus:outline-none"
-//       id="language-menu"
-//       aria-haspopup="true"
-//     >
-//       <span className="sr-only">Open language menu</span>
-//       <span>English</span>
-//       <svg className="ml-1 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-//         <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
-//       </svg>
-//     </button>
-//   </div>
-// );
 
 export default NavBar;
