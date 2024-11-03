37d64dddbade6e418fc067201b2bd55324eb8d90
myix765 <myix765@gmail.com>
2024-11-03 18:02:00 -0500

    clean up button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,13 +3,10 @@ import React from 'react';
 const Button = ({ isOutline, label }) => {
   return (
     <button
-      className={`px-4 py-2 font-semibold rounded-lg transition-colors duration-300 ${
-        isOutline
-          // when isOutline is true
-          ? 'border border-blue-500 text-black-500 bg-transparent hover:text-white'
-          // when isOutline is false
-          : 'bg-white text-black hover:bg-blue-700' 
-      }`}
+      className={`px-4 py-2 font-semibold rounded-lg transition-colors duration-300 ${isOutline
+        ? 'border border-blue-500 text-black-500 bg-white'
+        : 'bg-blue-700 text-black'
+        }`}
     >
       {label}
     </button>
