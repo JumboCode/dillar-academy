901f5b47e077ebcdaf250242ab6a4c5386ea09ca
Spencer Anderson <sanderson28801@gmail.com>
2024-11-15 16:43:36 -0500

    did frontend

--- src/components/PageRoutes.jsx
+++ src/components/PageRoutes.jsx
@@ -7,6 +7,7 @@ import Login from '@/pages/Login'
 import Classes from '@/pages/Classes'
 import Levels from '@/pages/Levels'
 import StyleGuide from "@/pages/StyleGuide";
+import Welcome from "../pages/Welcome";
 
 // TODO (John & Frank): add routes for Page Not Found and student and admin pages
 export default function PageRoutes() {
@@ -20,6 +21,7 @@ export default function PageRoutes() {
       <Route path="/levels" component={Levels} />
       <Route path="/classes" component={Classes} />
       <Route path="/style" component={StyleGuide} />
+      <Route path="/welcome" component={Welcome} />
     </Switch>
   );
 };
