9117bc5941978fb1473b3c95f057d7674927f9fb
Frank <frank38112@gmail.com>
2024-11-23 16:05:44 -0500

    changed button

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -8,22 +8,16 @@ const PageNotFound = () => {
                 <h1 className="flex items-center justify-center h-5/6 text-6xl mb-2 font-bold pt-5 py-5" >Page Not Found</h1>
                 <h2 className="flex items-center justify-center text-3xl mb-2 font-normal"> The page you're looking for doesn't exist.</h2>
                 <div className='flex items-center justify-center space-x-5 whitespace-nowrap pt-8'> 
-                    <button className="p-2 bg-indigo-950 px-5 text-white rounded-xl" onClick={
-                        () => {
-                            window.location.href = "/"; // Redirect to home page
-                        } }>
-                        Return Home
-                    </button>
-                    <button className="p-2 px-5 border-indigo-950 border-8" style={{
-                        borderRadius: '10px',
-                        borderWidth: '1px',
-                        borderColor: 'black'
-                    }} onClick={
-                        () => {
-                            window.location.href = "/"; // Redirect to home page
-                        } }>
-                        Get Help
-                    </button>
+                    <Button
+                        label={"Return Home"}
+                        onClick={() => window.location.href = "/"}
+                        isOutline={false}
+                    />
+                    <Button
+                        label={"Get Help"}
+                        onClick={() => window.location.href = "/"}
+                        isOutline={true}
+                    />
                 </div>
                 
             
