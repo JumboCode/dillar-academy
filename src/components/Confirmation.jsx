81a8cee467cacf2b8aaa2aff0962c0d6ab7de2b4
Aryaa Modi <aryaa.modi@tufts.edu>
2024-12-09 00:05:33 -0500

    completed body with proper sections and confirmation component

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -4,41 +4,45 @@ import Button from '@/components/Button/Button'
 
 const Class = ({ classObj }) => {
   const ageGroup = classObj.ageGroup.toString();
+  const day1 = classObj.schedule[0].day.toString();
+  const subDay1 = day1.substr(0,3);
+
+  const time = classObj.schedule[0].time.toString();
+
+  const day2 = classObj.schedule[1].day.toString();
+  const subDay2 = day2.substr(0,3);
+
 
   return (
-    <div className="p-6 bg-white rounded-lg shadow-shadow overflow-hidden hover:shadow-shadow-hover transition-shadow">
-      <div className="flex flex-col gap-4">
+    <div className="p-6 bg-white rounded-lg shadow-shadow overflow-hidden hover:shadow-shadow-hover transition-shadow w-96">
+      <div className="flex flex-col gap-2">
         {/* Header */}
 
         <div>
-            <h3>You are registered!</h3>
+            <h3 className="text-2xl font-extrabold mb-2">You are registered!</h3>
             <p>
             {ageGroup.charAt(0).toUpperCase() + ageGroup.slice(1)}'s Class w/ {classObj.instructor}
             </p>
         </div>
 
         {/* Schedule */}
-        <div className="space-y-3">
-          {classObj.schedule.map((schedule, index) => (
-            <div key={index} className="flex items-center text-neutral-400 text-sm">
-              <div className="flex items-center gap-2 w-1/2">
-                <IoCalendarOutline className="text-lg" />
-                <span>{schedule.day}</span>
-              </div>
-              <div className="flex items-center gap-2">
-                <IoTimeOutline className="text-lg" />
-                <span>{schedule.time}</span>
-              </div>
-            </div>
-          ))}
+        <div className="flex flex-col text-neutral-400 text-sm mb-2">
+          <div className="flex items-left gap-2">
+            <IoCalendarOutline className="text-lg" />
+            <span>{subDay1} & {subDay2}</span>
+          </div>
+          <div className="flex items-left gap-2">
+            <IoTimeOutline className="text-lg" />
+            <span>{time}</span>
+          </div>
         </div>
 
-        <div className='grid grid-cols-2 gap-3'>
-          <Button
-            label={"Got it"}
-            onClick={() => alert("dummy button clicked!")}
-            isOutline={false}
-          />
+        <div className='gap-3 w-full'>
+          <button
+            className={`px-4 py-2 rounded-lg transition-colors duration-300 border border-dark-blue-800 w-full`}
+          >
+          Got it!
+          </button>
         </div>
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,8 @@
-
-import dummyBg from '@/assets/dummy-bg.png';
 import Button from '@/components/Button/Button';
 import Level from '@/components/Level.jsx';
 import Class from '@/components/Class.jsx';
 import Confirmation from '@/components/Confirmation.jsx';
 import { useLocation } from 'wouter';
-
-const sections = [
-    {
-        imgSrc: dummyBg,
-        imgAlt: "dummy-bg1",
-        title: "Choose your class level",
-        description: "Take a quick assessment to see what your English level is like!",
-    },
-    {
-        imgSrc: dummyBg,
-        imgAlt: "dummy-bg2",
-        title: "Find a time and instructor",
-        description: "Browse our list of classes to find one that works!",
-    },
-    {
-        imgSrc: dummyBg,
-        imgAlt: "dummy-bg3",
-        title: "Start Learning",
-        description: "Attend class and start learning right away!",
-    },
-];
 
 const class1 = {
     level: "1",
@@ -85,42 +62,44 @@ const Home = () => {
                 </div>
             </div>
 
-            <div className="bg-white p-4 mt-4">
-                {/* <h1 className="text-4xl font-extrabold mb-10 text-center">How It Works</h1> */}
-                {sections.map((section, index) => (
-                    <div key={index} className={`flex flex-col md:flex-row items-center justify-evenly px-4 md:px-2 mb-4 ${index % 2 === 1 ? 'md:flex-row-reverse' : ''}`}>
-                    
-                        {section.title === "Choose your class level" && (
-                            <div className="flex flex-col items-center  md:flex-row space-x-4 md:w-2/3">
-                                <Level level="1" isSimplified={true} className="w-48 h-32 md:w-56 md:h-40"/>
-                                <Level level="2" isSimplified={true} className="w-48 h-32 md:w-56 md:h-40"/>
-                            </div>
-                        )}
+            <div className="flex flex-col bg-white p-4  gap-32 m-40">
+                <div className="flex flex-row justify-around items-center">
+                    <div className="flex flex-col items-center md:flex-row space-x-4">
+                        <Level level="1" isSimplified={true} className="w-48 h-32 md:w-56 md:h-40"/>
+                        <Level level="2" isSimplified={true} className="w-48 h-32 md:w-56 md:h-40"/>
+                    </div>                    
 
-                        {section.title === "Find a time and instructor" && (
-                            <div className="flex flex-col items-center  md:flex-row space-x-4 md:w-2/3">
-                                <Class classObj={class1} className="w-48 h-32 md:w-56 md:h-40"/>
-                                <Class classObj={class2} className="w-48 h-32 md:w-56 md:h-40"/>
-                            </div>
-                        )}
-
-                        {/* could be slower with the if checks so we shouldn't do it like that */}
-                        {section.title === "Start Learning" && (
-                            <div className="flex flex-col items-center  md:flex-row space-x-4 md:w-2/3">
-                                <Confirmation classObj={class1} className="w-48 h-32 md:w-56 md:h-40"/>
-                            </div>
-                        )}  
-
-                        {/* <img src={section.imgSrc} alt={section.imgAlt} className="mb-4 md:mb-8 md:w-1/3 rounded-lg shadow-md" /> */}
-                        <div className={`px-2 md:px-0 text-center md:text-left md:w-1/3`}>
-                            <h2 className="text-4xl font-semibold mb-5">{section.title}</h2>
-                            <p className="text-2xl mb-4">{section.description}</p>
-                        </div>
+                    <div className={`px-2 md:px-0 text-center w-[484px] md:text-left`}>
+                        <h2 className="text-4xl font-semibold mb-5">Choose your class level</h2>
+                        <p className="text-2xl mb-4">We offer multiple levels of English classes based on your experience.</p>
                     </div>
-                ))}
+                </div>
+
+                <div className="flex flex-row justify-around items-center">
+                    <div className={`px-2 md:px-0 text-center w-[484px] md:text-left`}>
+                        <h2 className="text-4xl font-semibold mb-5">Find a time and instructor</h2>
+                        <p className="text-2xl mb-4">Choose a class time that works for you or an instructor who’s teaching style you like!</p>
+                    </div>
+
+                    <div className="flex flex-col items-center md:flex-row space-x-4">
+                        <Class classObj={class1} className="w-48 h-32 md:w-56 md:h-40"/>
+                        <Class classObj={class2} className="w-48 h-32 md:w-56 md:h-40"/>
+                    </div>
+                </div>
+
+                <div className="flex flex-row justify-around items-center">
+                    <div className="flex flex-col items-center  md:flex-row space-x-4">
+                        <Confirmation classObj={class1} className="w-48 h-32 md:w-56 md:h-40"/>
+                    </div>
+
+                    <div className={`px-2 md:px-0 text-center w-[484px] md:text-left`}>
+                        <h2 className="text-4xl font-semibold mb-5">Start Learning</h2>
+                        <p className="text-2xl mb-4">Attend class and start learning right away!</p>
+                    </div>
+                </div>
             </div>
+
             <footer className="h-80 bg-dark-blue-800 text-white flex flex-col justify-center gap-y-5">
-                {/* is there a better way of doing this so that mx-20 isn't repeated 3 times? */}
                 <p className="text-4xl font-extrabold mx-20">Dillar English Academy</p>
                 <p className="text-lg mx-20">Email: dillarenglish@gmail.com</p>
                 <p className = "text-lg mx-20">Instagram: @dillaracademy</p>
