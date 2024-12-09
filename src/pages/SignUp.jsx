1a5f9e7544e77fb717ce30927774bde4037fcfd5
Donatello <donatello.sassaroli@tufts.edu>
2024-12-08 19:26:54 -0500

    password req translations

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -118,6 +118,13 @@ export default function SignUp() {
               value={formData.password}
               valueAgain={formData.retypedPassword}
               onChange={(isValid) => setIsValid(isValid)}
+              messages={{
+                minLength: t("password_qual1_text"),
+                capitalAndLowercase: t("password_qual2_text"),
+                number: t("password_qual3_text"),
+                specialChar: t("password_qual4_text"),
+                match: t("password_qual5_text"),
+              }}
             />
           </div>
           <FormSubmit label={t("sign_up_text")} isDisabled={!isValid} />
