879aa8051caa11ef0f68c3dfd29e16604739572d
Madeline Lei <madeline.lei@gmail.com>
2025-02-02 14:54:12 -0500

    finished second part

--- src/pages/dashboards/AdminView.jsx
+++ src/pages/dashboards/AdminView.jsx
@@ -2,12 +2,14 @@ import { useContext, useEffect, useState } from "react";
 import { UserContext } from '@/contexts/UserContext.jsx';
 import { useLocation } from 'wouter';
 import { useAuth } from '@clerk/clerk-react'
+import { getUsers } from '../../api/user-wrapper'
 
 const AdminView = () => {
   const { user, } = useContext(UserContext);
   const [, setLocation] = useLocation();
   const { isSignedIn, isLoaded } = useAuth();
   const [allowRender, setAllowRender] = useState(false);
+  const [users, setUsers] = useState([]);
 
   useEffect(() => {
     if (isLoaded) {
@@ -17,6 +19,12 @@ const AdminView = () => {
         setAllowRender(true);
       }
     }
+
+    const fetchData = async () => {
+      const userData = await getUsers();
+      setUsers(userData.data);
+    }
+    fetchData();
   }, [isLoaded, isSignedIn, user]);
 
   if (!allowRender) {
@@ -29,7 +37,28 @@ const AdminView = () => {
 
   return (
     <div className="h-full">
-      <h1>Admin</h1>
+      <h1 class="text-xl py-4 px-2">Admin</h1>
+      <table class="table-auto w-full text-left">
+        <thead class="bg-neutral-200 text-lg">
+          <tr>
+            <th class="px-3">Name</th>
+            <th class="px-3">Email</th>
+            <th class="px-3">Password</th>
+            <th class="px-3">Privilege</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {users.map((user) => (
+            <tr class="border-b">
+              <td class="py-2 px-3">{user.firstName} {user.lastName}</td>
+              <td class="py-2 px-3">{user.email}</td>
+              <td class="py-2 px-3">{user.password}</td>
+              <td class="py-2 px-3">{user.privilege}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
