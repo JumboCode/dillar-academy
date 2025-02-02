c6dbbd6d202ab348547fd7c071074373bdf769fa
Madeline Lei <madeline.lei@gmail.com>
2025-02-02 18:27:27 -0500

    last changes

--- src/pages/dashboards/AdminView.jsx
+++ src/pages/dashboards/AdminView.jsx
@@ -37,24 +37,24 @@ const AdminView = () => {
 
   return (
     <div className="h-full">
-      <h1 class="text-xl py-4 px-2">Admin</h1>
-      <table class="table-auto w-full text-left">
-        <thead class="bg-neutral-200 text-lg">
+      <h1 className="text-xl py-4 px-2">Admin</h1>
+      <table className="table-auto w-full text-left">
+        <thead className="bg-neutral-200 text-lg">
           <tr>
-            <th class="px-3">Name</th>
-            <th class="px-3">Email</th>
-            <th class="px-3">Password</th>
-            <th class="px-3">Privilege</th>
+            <th className="px-3">Name</th>
+            <th className="px-3">Email</th>
+            <th className="px-3">Password</th>
+            <th className="px-3">Privilege</th>
           </tr>
         </thead>
 
         <tbody>
           {users.map((user) => (
-            <tr class="border-b">
-              <td class="py-2 px-3">{user.firstName} {user.lastName}</td>
-              <td class="py-2 px-3">{user.email}</td>
-              <td class="py-2 px-3">{user.password}</td>
-              <td class="py-2 px-3">{user.privilege}</td>
+            <tr className="border-b">
+              <td className="py-2 px-3">{user.firstName} {user.lastName}</td>
+              <td className="py-2 px-3">{user.email}</td>
+              <td className="py-2 px-3">{user.password}</td>
+              <td className="py-2 px-3">{user.privilege}</td>
             </tr>
           ))}
         </tbody>
