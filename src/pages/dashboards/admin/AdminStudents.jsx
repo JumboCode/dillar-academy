54ceb9fd21ca6195bfb27104c8d48a4a19d917f0
Spencer Anderson <sanderson28801@gmail.com>
2025-02-26 00:49:29 -0500

    got header working

--- src/pages/dashboards/admin/AdminStudents.jsx
+++ src/pages/dashboards/admin/AdminStudents.jsx
@@ -4,7 +4,8 @@ import { useLocation } from 'wouter';
 import { useAuth } from '@clerk/clerk-react';
 import { getUsers } from '@/api/user-wrapper.js'
 import Button from '@/components/Button/Button';
-
+import Dropdown from '@/components/Dropdown/Dropdown';
+import { IoSearch , IoPersonOutline  } from "react-icons/io5";
 const AdminStudents = () => {
   const { user } = useContext(UserContext);
   const [, setLocation] = useLocation();
@@ -42,8 +43,29 @@ const AdminStudents = () => {
     <div className="h-full p-8 space-y-7">
       <h3 className="font-extrabold">Students</h3>
       <p>List of all students enrolled in Dillar Classes</p>
-      <input type="text" className=""placeholder="Search names, levels, or classes..."></input>
-      <section>
+      <div className="w-full inline-flex">
+        <div className="w-[91%] inline-flex items-center p-1 pl-2 gap-3 border border-black rounded-xs border-gray-300">
+          <IoSearch size={16.81} className="text-gray-400"/>
+          <input type="text" className="w-5/6 border-none outline-none text-[18px]" placeholder="Search names, levels, or classes..."></input>
+        </div>
+        <div className="inline-flex">
+          <Dropdown
+            label={
+                  <div className="flex items-center justify-center gap-x-1">
+                    <span className="text-[18px] pt-2">Filter By</span>
+                  </div>
+            }
+            buttonClassName="flex items-center justify-center w-full text-base font-normal text-black min-w-fit   sm:px-5 gap-1 rounded-lg bg-white"
+          ></Dropdown>
+        </div>
+        
+      </div>
+      <div className="text-indigo-900 inline-flex text-[18px] items-center ">
+          <IoPersonOutline size={18.43}/>
+          <p>{users.length} students</p>
+      </div>      
+      
+      {/* <section>
        
         <table className="table-auto w-full text-left">
           <thead className="bg-neutral-200 text-lg">
@@ -73,7 +95,7 @@ const AdminStudents = () => {
             ))}
           </tbody>
         </table>
-      </section>
+      </section> */}
     </div>
   )
 }
