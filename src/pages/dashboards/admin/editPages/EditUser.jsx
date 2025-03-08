7868ae94a00dc56d68fd4cb0d957b92cc26199f6
myix765 <myix765@gmail.com>
2025-03-08 14:59:50 -0500

    style edit user view

--- src/pages/dashboards/admin/editPages/EditUser.jsx
+++ src/pages/dashboards/admin/editPages/EditUser.jsx
@@ -2,16 +2,19 @@ import { useContext, useEffect, useState } from "react";
 import { UserContext } from '@/contexts/UserContext.jsx';
 import { useLocation, useParams } from 'wouter';
 import { useAuth } from '@clerk/clerk-react';
-import { updateUser, getUser } from '@/api/user-wrapper.js'
+import { updateUser, getUser } from '@/api/user-wrapper.js';
+import { getClassById } from '@/api/class-wrapper';
 import FormInput from '@/components/Form/FormInput'
 import Button from '@/components/Button/Button';
+import BackButton from "@/components/Button/BackButton";
+import Class from '@/components/Class/Class';
 
 const EditUser = () => {
   const { user } = useContext(UserContext);
   const [, setLocation] = useLocation();
   const { isSignedIn, isLoaded } = useAuth();
   const [allowRender, setAllowRender] = useState(false);
-
+  const [userData, setUserData] = useState(null);
   const params = useParams();
   const [userFormData, setUserFormData] = useState({
     firstName: '',
@@ -41,6 +44,14 @@ const EditUser = () => {
         email: userData.data.email,
         password: userData.data.password
       });
+      const classes = await Promise.all(
+        userData.data.enrolledClasses.map(async (classID) => {
+          const classResponse = await getClassById(classID);
+          return classResponse; // Return the class details
+        })
+      );
+      userData.data.enrolledClasses = classes;
+      setUserData(userData.data);
     } catch (error) {
       console.error('Error fetching user:', error);
     }
@@ -61,7 +72,7 @@ const EditUser = () => {
     }
   };
 
-  if (!allowRender) {
+  if (!allowRender || !userData) {
     return <div></div>;
   }
 
@@ -70,37 +81,55 @@ const EditUser = () => {
   }
 
   return (
-    <div className="page-format">
-      <h3 className="font-extrabold mb-10">Edit User</h3>
-      <form onSubmit={handleEditUser} className="space-y-3">
-        <FormInput
-          type="text"
-          name="firstName"
-          placeholder="First Name"
-          value={userFormData.firstName}
-          onChange={handleUserInputChange}
-          isRequired={true}
-        />
-        <FormInput
-          type="text"
-          name="lastName"
-          placeholder="Last Name"
-          value={userFormData.lastName}
-          onChange={handleUserInputChange}
-          isRequired={true}
-        />
-        <FormInput
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={userFormData.email}
-          onChange={handleUserInputChange}
-          isRequired={true}
-        />
-        <div className="flex justify-end space-x-2">
-          <Button label="Save" type="submit" />
+    <div className="page-format space-y-12">
+      {/* <BackButton label={"Class"} href={"/admin/levels/conversations/"} /> */}
+      <h3 className="font-extrabold">{userData.firstName + " " + userData.lastName}</h3>
+      <form onSubmit={handleEditUser} className="space-y-12">
+        <div className="flex w-full gap-x-6">
+          <div className="w-full">
+            <label>First Name</label>
+            <FormInput
+              type="text"
+              name="firstName"
+              placeholder="First Name"
+              value={userFormData.firstName}
+              onChange={handleUserInputChange}
+              isRequired={true}
+            />
+          </div>
+          <div className="w-full">
+            <label>Last Name</label>
+            <FormInput
+              type="text"
+              name="lastName"
+              placeholder="Last Name"
+              value={userFormData.lastName}
+              onChange={handleUserInputChange}
+              isRequired={true}
+            />
+          </div>
+          <div className="w-full">
+            <label>Email</label>
+            <FormInput
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={userFormData.email}
+              onChange={handleUserInputChange}
+              isRequired={true}
+            />
+          </div>
         </div>
       </form>
+      <Button label="Save" type="submit" />
+      <div>
+        <h4 className="font-extrabold mb-6">{userData.firstName}'s Classes</h4>
+        <div className="grid grid-cols-3 gap-6">
+          {userData.enrolledClasses.map((classObj) => (
+            <Class key={classObj._id} classObj={classObj} modes={["edit"]} editURL="" />
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
