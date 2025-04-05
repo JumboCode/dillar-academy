c9db6b22b854ac71bb2927bf2f42ac8023d70141
myix765 <myix765@gmail.com>
2025-04-05 11:33:48 -0400

    change footer color shh

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,12 +5,12 @@ const Footer = () => {
   const { t } = useTranslation();
 
   return (
-    <footer className="h-60 lg:h-80 bg-blue-600 text-white flex flex-col justify-center items-center gap-y-3 sm:gap-y-5">
-      <div className="max-w-[96rem] w-full px-4 sm:px-6 lg:px-20 space-y-3 sm:space-y-5">
+    <footer className="h-60 lg:h-80 bg-blue-700 text-white flex flex-col justify-center items-center gap-y-3 sm:gap-y-5">
+      <div className="max-w-[96rem] w-full px-4 sm:px-6 lg:px-20 flex flex-col gap-y-3 sm:gap-y-5">
         <h1 className="font-extrabold">Dillar Academy</h1>
         <p className="lg:text-lg">{t("email_field")}: dillarenglish@gmail.com</p>
         <p className="lg:text-lg">{t("instagram")}: @dillaracademy</p>
-        <Link href="/contact">Contact Us</Link>
+        <Link href="/contact" className={"lg:text-lg"}>Contact Us</Link>
       </div>
     </footer>
   )
