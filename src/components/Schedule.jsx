f39ea79e13882a48a9dd5fe75de2cff44fb73834
myix765 <myix765@gmail.com>
2025-04-09 02:17:34 -0400

    use schedule component in admin schedule

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -3,7 +3,7 @@ import { UserContext } from '@/contexts/UserContext.jsx';
 import { useLocation, Link } from 'wouter';
 import Button from '@/components/Button/Button';
 
-const Schedule = ({ classes }) => {
+const Schedule = ({ classes, filters = [] }) => {
   const { user } = useContext(UserContext);
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 640);
 
@@ -52,24 +52,34 @@ const Schedule = ({ classes }) => {
                     classroomLink: classObj.classroomLink
                   })))
                 .filter(schedule => schedule.day.slice(0, 3).toUpperCase() === day)
+                .filter(schedule => filters.length === 0 || filters.includes(schedule.level))
                 .sort((a, b) => new Date(`1970/01/01 ${a.time}`) - new Date(`1970/01/01 ${b.time}`)) // Sort by time
                 .map((classObj, index) => {
                   const classElement = <ScheduleClass key={index} classObj={classObj} isMobile={isMobile} />;
 
-                  if (isMobile && user.privilege === "instructor") {
-                    return (
-                      <Link key={index} to={`/instructor/class/${classObj._id}`}>
-                        {classElement}
-                      </Link>
-                    );
-                  }
-
-                  if (isMobile && user.privilege === "student") {
-                    return (
-                      <a key={index} href={classObj.classroomLink}>
-                        {classElement}
-                      </a>
-                    );
+                  if (isMobile) {
+                    switch (user.privilege) {
+                      case "admin":
+                        return (
+                          <Link key={index} to={`/admin/class/${classObj._id}`}>
+                            {classElement}
+                          </Link>
+                        );
+                      case "instructor":
+                        return (
+                          <Link key={index} to={`/instructor/class/${classObj._id}`}>
+                            {classElement}
+                          </Link>
+                        );
+                      case "student":
+                        return (
+                          <a key={index} href={classObj.classroomLink}>
+                            {classElement}
+                          </a>
+                        );
+                      default:
+                        return classElement;
+                    }
                   }
 
                   return classElement;
@@ -85,20 +95,26 @@ const Schedule = ({ classes }) => {
 const ScheduleClass = ({ classObj, isMobile }) => {
   const { user } = useContext(UserContext);
   const [, setLocation] = useLocation();
-  console.log(isMobile)
-
   return (
-    <div className="bg-blue-100 rounded-sm border-[0.5px] border-gray-200 p-1 sm:p-3">
+    <div className="bg-blue-100 rounded-sm border-[0.5px] border-gray-200 p-1 sm:p-3 mb-2">
       <p className="text-blue-700 text-[0.75rem] sm:text-[0.875rem]">{classObj.time}</p>
       <p className="font-extrabold text-[0.75rem] sm:text-[0.875rem] sm:mt-2">Level {classObj.level}</p>
       <p className="text-gray-800 text-[0.675rem] sm:text-xs sm:mb-3">{classObj.ageGroup === "all" ? "ALL AGES" : `${classObj.ageGroup.toUpperCase()}'s CLASS`}</p>
       {!isMobile && (
-        user.privilege === "instructor" ? (
-          <Button label="Edit" onClick={() => setLocation(`/instructor/class/${classObj._id}`)} />
-        ) : (
+        user.privilege === "student" ? (
           <a href={classObj.classroomLink}>
             <Button label="Join" onClick={null} />
           </a>
+        ) : (
+          <Button
+            label="Edit"
+            onClick={() => {
+              const basePath = user.privilege === "admin"
+                ? `/admin/class/${classObj._id}`
+                : `/instructor/class/${classObj._id}`;
+              setLocation(basePath);
+            }}
+          />
         )
       )}
     </div>
