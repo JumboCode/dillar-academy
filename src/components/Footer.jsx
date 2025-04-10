9e00f5aa27b22fccf75e38b84e727ec4ec20a79e
myix765 <myix765@gmail.com>
2025-04-10 18:47:33 -0400

    add links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,8 +8,12 @@ const Footer = () => {
     <footer className="h-60 lg:h-80 bg-blue-700 text-white flex flex-col justify-center items-center gap-y-3 sm:gap-y-5">
       <div className="max-w-[96rem] w-full px-4 sm:px-6 lg:px-20 flex flex-col gap-y-3 sm:gap-y-5">
         <h1 className="font-extrabold">Dillar Academy</h1>
-        <p className="lg:text-lg">{t("email_field")}: dillarenglish@gmail.com</p>
-        <p className="lg:text-lg">{t("instagram")}: @dillaracademy</p>
+        <a href="mailto:dillaracademy@gmail.com">
+          <p className="lg:text-lg">{t("email_field")}: dillaracademy@gmail.com</p>
+        </a>
+        <a href={'https://www.instagram.com/dillaracademy/'} target="_blank" rel="noreferrer noopener">
+          <p className="lg:text-lg">{t("instagram")}: @dillaracademy</p>
+        </a>
         <Link href="/contact" className={"lg:text-lg"}>Contact Us</Link>
       </div>
     </footer>
