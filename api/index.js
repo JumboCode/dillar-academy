341b0275926396a4809b39fce74510abc0a42cf0
claireslee <claireslee6@gmail.com>
2025-04-17 18:04:02 -0400

    admin edit user privs, delete users

--- api/index.js
+++ api/index.js
@@ -1036,3 +1036,26 @@ app.get('/api/students-export', async (req, res) => {
     res.status(500).json({ message: 'Error exporting students' });
   }
 });
+
+// Delete User
+app.delete('/api/user/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ error: 'Invalid ID' });
+    }
+
+    const deletedUser = await User.findOne({ _id: id });
+    if (!deletedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // delete user
+    await User.findByIdAndDelete(id);
+
+    res.status(204).json({ message: 'User deleted successfully' });
+  } catch (error) {
+    console.error('Failed to delete user class:', error);
+    res.status(500).json({ message: 'Failed to delete user class' });
+  }
+});
