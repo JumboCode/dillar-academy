1c4f1c42cf32ef0b33a6dc58b49bdbc79b33cb25
myix765 <myix765@gmail.com>
2025-05-30 13:46:21 -0400

    edit class change fetch to use getClassById

--- src/pages/dashboards/admin/editPages/EditClass.jsx
+++ src/pages/dashboards/admin/editPages/EditClass.jsx
@@ -10,7 +10,7 @@ import DayDropdown from '@/components/Dropdown/DayDropdown';
 import UserItem from "@/components/UserItem";
 import Alert from '@/components/Alert';
 import { IoAdd, IoTrashBinOutline, IoPersonOutline } from "react-icons/io5";
-import { updateClass, deleteClass, getClasses } from '@/wrappers/class-wrapper';
+import { updateClass, deleteClass, getClasses, getClassById } from '@/wrappers/class-wrapper';
 import { getUser } from '@/wrappers/user-wrapper';
 import Unauthorized from "@/pages/Unauthorized";
 import SkeletonUser from "@/components/Skeletons/SkeletonUser";
@@ -27,7 +27,6 @@ const EditClass = () => {
   const [isSaving, setIsSaving] = useState(false);
 
   const params = useParams();
-  const [classes, setClasses] = useState(null);
   const [classObj, setClassObj] = useState(null);
   const [classData, setClassData] = useState({
     level: '',
@@ -60,9 +59,7 @@ const EditClass = () => {
 
   const fetchClass = async () => {
     if (user) {
-      const data = await getClasses();
-      setClasses(data);
-      const classObj = data.find(c => c._id === params.classId); //TODO: just use getClassById?
+      const classObj = await getClassById(params.classId);
       setClassObj(classObj);
       setClassData({
         level: classObj.level,
